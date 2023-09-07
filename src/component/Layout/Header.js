c68e851f34a16fc64e177614f6e267bee5ca9fa6
Taha Ratlam Wala <56840043+taharatlam@users.noreply.github.com>
2023-09-07 19:35:38 +0530

    chnages002

--- src/component/Layout/Header.js
+++ src/component/Layout/Header.js
@@ -5,6 +5,13 @@ import Link from "next/link";
 import Image from "next/image";
 import styled from 'styled-components';
 import CustomCursor from "../CustomCursor";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faCircleStop,
+  faBars,
+  faXmark
+} from "@fortawesome/free-solid-svg-icons";
 
 const Header = (props) => {
   const servicesType = [...services.serviceType];
@@ -46,6 +53,7 @@ const Header = (props) => {
   return (
     <>
     <CustomCursor />
+    
       <header id="ori-header" className={`ori-header-section header-style-one ${isSticky ? 'sticky-on' : ''}`}>
         <div className="ori-header-content-area">
           <div className="ori-header-content d-flex align-items-center justify-content-between">
@@ -108,25 +116,29 @@ const Header = (props) => {
             <div className="ori-header-sidebar-search d-flex align-items-center">
               <div className="ori-search-btn">
                 <button className="search-box-outer" onClick={()=>setIsSearchActive(true)}>
-                  <i className="fal fa-search"></i>
+                  {/* <i className="fal fa-search"></i> */}
+                    <FontAwesomeIcon icon={faMagnifyingGlass}/>
                 </button>
               </div>
               <div className="ori-sidenav-btn navSidebar-button">
                 <button onClick={()=>setIsActive(true)}>
-                  <i className="fal fa-bars"></i>
+                  {/* <i className="fal fa-bars"></i> */}
+                  <FontAwesomeIcon icon={faBars} />
                 </button>
               </div>
             </div>
           </div>
           <div className="mobile_menu position-relative">
             <div className="mobile_menu_button open_mobile_menu">
-              <i className="fal fa-bars"></i>
+              {/* <i className="fal fa-bars"></i> */}
+              <FontAwesomeIcon icon={faBars} />
             </div>
-            <div className="mobile_menu_wrap">
-              <div className="mobile_menu_overlay open_mobile_menu"></div>
+            <div className={`mobile_menu_wrap ${isMobileMenu&&"mobile_menu_on"}`}>
+              <div className={`mobile_menu_overlay open_mobile_menu`}></div>
               <div className="mobile_menu_content">
                 <div className="mobile_menu_close open_mobile_menu">
-                  <i className="fal fa-times"></i>
+                  {/* <i className="fal fa-times"></i> */}
+                  <FontAwesomeIcon icon={faXmark} />
                 </div>
                 <div className="m-brand-logo">
                   <Link href="%21.html#">
@@ -142,7 +154,8 @@ const Header = (props) => {
       {/* search */}
       <div className="search-popup">
         <button className="close-search style-two" onClick={()=>setIsSearchActive(false)}>
-          <span className="fal fa-times"></span>
+          {/* <span className="fal fa-times"></span> */}
+          <FontAwesomeIcon icon={faXmark} />
         </button>
         <button className="close-search" onClick={()=>setIsSearchActive(false)}>
           <span className="fa fa-arrow-up"></span>
